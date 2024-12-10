53fd0a85e1fad31f31c3c3f08fbaf754be0369e8
dngan0365 <dngan0365@gmail.com>
2024-12-11 01:03:12 +0700

    add file index for schedule

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
 import webhookRouter from "./routes/webhook.route.js";
 import chatRouter from "./routes/chat.route.js"
+import scheduleRouter from "./routes/schedule.route.js"
 import { clerkMiddleware, requireAuth } from "@clerk/express";
 import cors from "cors";
 import url, { fileURLToPath } from "url";
@@ -61,6 +62,7 @@ app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 app.use("/chats", chatRouter)
+app.use("/schedule", scheduleRouter)
 
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
